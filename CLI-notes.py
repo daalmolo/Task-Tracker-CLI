4ff76ed117588bf601d40f84312f6077ab88502c
daalmolo <daniel.daalmolo@gmail.com>
2024-11-15 18:00:18 -0500

    Commands now take arguments

--- CLI-notes.py
+++ CLI-notes.py
@@ -4,32 +4,32 @@ app = typer.Typer()
 
 
 @app.command()
-def add():
+def add(task_string: str):
     pass
 
 
 @app.command()
-def update():
+def update(index: int, updated_string: str):
     pass
 
 
 @app.command()
-def delete():
+def delete(index: int):
     pass
 
 
 @app.command()
-def mark_in_progress():
+def mark_in_progress(index: int):
     pass
 
 
 @app.command()
-def mark_done():
+def mark_done(index: int):
     pass
 
 
 @app.command()
-def list_tasks():
+def list_tasks(status_query: str = "all"):
     pass
 
 
