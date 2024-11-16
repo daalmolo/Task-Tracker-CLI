97fb68de7b33fff2917344ab44330a98d34b3420
daalmolo <daniel.daalmolo@gmail.com>
2024-11-15 23:53:18 -0500

    The intended functionality was implemented

--- database_CLI.py
+++ database_CLI.py
@@ -4,18 +4,115 @@ db_app = sqlite3.connect("notes_db.db")
 
 notes_db_cursor = db_app.cursor()
 
-var_test = False
-
 notes_db_cursor.execute(
     """
     create table if not exists tasks_table (
     task_string text not null,
-    done INTEGER NOT NULL CHECK (is_active IN (0, 1)) DEFAULT 0, 
-    in_progress INTEGER NOT NULL CHECK (is_active IN (0, 1)) DEFAULT 0, 
+    done varchar(5) NOT NULL DEFAULT 'False',
+    in_progress varchar(5) NOT NULL DEFAULT 'False'
     )
     """
 )
 
 
+def add_record(task):
+    notes_db_cursor.execute(
+        """
+        insert into tasks_table (task_string) values (?)
+        """,
+        (task,),
+    )
+    db_app.commit()
+
+
+def update_record(up_string, index):
+    notes_db_cursor.execute(
+        """
+        update tasks_table
+        set task_string = (?)
+        where rowid = (?)
+        """,
+        (up_string, index),
+    )
+    db_app.commit()
+
+
+def delete_record(index):
+    notes_db_cursor.execute(
+        """
+        delete from tasks_table where rowid = (?)
+        """,
+        (index,),
+    )
+    db_app.commit()
+
+
+def update_in_progress(index):
+    notes_db_cursor.execute(
+        """
+        update tasks_table
+        set in_progress = 'True'
+        where rowid = (?)
+        """,
+        (index,),
+    )
+    db_app.commit()
+
+
+def update_done(index):
+    notes_db_cursor.execute(
+        """
+        update tasks_table
+        set done = 'True', in_progress = 'False'
+        where rowid = (?)
+        """,
+        (index,),
+    )
+    db_app.commit()
+
+
+def list_records(constraint):
+    match constraint:
+        case "all":
+            notes_db_cursor.execute(
+                """
+                select rowid, * from tasks_table 
+                """
+            )
+        case "done":
+            notes_db_cursor.execute(
+                """
+                select rowid, * from tasks_table 
+                where done = "True"
+                """
+            )
+        case "todo":
+            notes_db_cursor.execute(
+                """
+                select rowid, * from tasks_table 
+                where done = "False" and in_progress = "False"
+                """
+            )
+        case "in-progress":
+            notes_db_cursor.execute(
+                """
+                select rowid, * from tasks_table 
+                where in_progress = "True"
+                """
+            )
+        case _:
+            exit()
+
+    print("| ID \t| TASK \t\t| DONE \t| IN PROGRESS |")
+
+    task_formatted_string = ""
+
+    for task in notes_db_cursor.fetchall():
+        for item in task:
+            task_formatted_string += f"| {item}\t"
+
+        print(f"{task_formatted_string} \t|")
+        task_formatted_string = ""
+
+
 db_app.commit()
-db_app.close()
--- notes_CLI.py
+++ notes_CLI.py
@@ -1,4 +1,12 @@
 import typer
+from database_CLI import (
+    add_record,
+    update_record,
+    delete_record,
+    update_in_progress,
+    list_records,
+    update_done,
+)
 
 
 app = typer.Typer()
@@ -6,32 +14,34 @@ app = typer.Typer()
 
 @app.command()
 def add(task_string: str):
-    pass
+    add_record(task_string)
+
+    print("Task added to the database")
 
 
 @app.command()
-def update(index: int, updated_string: str):
-    pass
+def update(updated_string: str, index: int):
+    update_record(updated_string, index)
 
 
 @app.command()
 def delete(index: int):
-    pass
+    delete_record(index)
 
 
 @app.command()
 def mark_in_progress(index: int):
-    pass
+    update_in_progress(index)
 
 
 @app.command()
 def mark_done(index: int):
-    pass
+    update_done(index)
 
 
 @app.command()
-def list_tasks(status_query: str = "all"):
-    pass
+def list_tasks(status: str):
+    list_records(status)
 
 
 if __name__ == "__main__":
